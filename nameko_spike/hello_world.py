24887321a8e0a091b148b95a3801c4e6b04a661e
Radosław Szalski <radoslaw.szalski@gmail.com>
2015-08-02 22:26:55 +0200

    Adds a BROADCAST event dispatcher and listener

--- nameko_spike/hello_world.py
+++ nameko_spike/hello_world.py
@@ -3,6 +3,7 @@ Hello World Nameko Microservice
 """
 import random
 
+from nameko.events import event_handler, EventDispatcher, BROADCAST
 from nameko.rpc import rpc, RpcProxy
 
 
@@ -32,3 +33,29 @@ class NameBuilder(object):
         title = random.choice(self.available_titles)
 
         return '{} {}'.format(name, title)
+
+
+class WatchDog(object):
+    name = 'watchdog'
+
+    dispatch = EventDispatcher()
+
+    @rpc
+    def bark(self):
+        self.dispatch('bark', 'Woof!')
+
+
+class SleepyGuard(object):
+    name = 'sleepy_guard'
+
+    @event_handler('watchdog', 'bark', handler_type=BROADCAST, reliable_delivery=False)
+    def wake_up(self, payload):
+        print('Oi! I\'m not sleeping, no need to bark!')
+
+
+class AwareGuard(object):
+    name = 'aware_guard'
+
+    @event_handler('watchdog', 'bark', handler_type=BROADCAST, reliable_delivery=False)
+    def on_bark(self, payload):
+        print('Good boy!')
