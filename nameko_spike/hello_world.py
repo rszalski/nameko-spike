77d95f9f17842a64b88742047fbd7f9c070e5822
Radosław Szalski <radoslaw.szalski@gmail.com>
2015-08-02 21:56:36 +0200

    Adds a dependant RPC Service for title formatting

--- nameko_spike/hello_world.py
+++ nameko_spike/hello_world.py
@@ -1,13 +1,34 @@
 """
 Hello World Nameko Microservice
 """
+import random
 
-from nameko.rpc import rpc
+from nameko.rpc import rpc, RpcProxy
 
 
 class GreetingService(object):
     name = 'greeting_service'
 
+    title_builder = RpcProxy('title_builder')
+
     @rpc
     def hello(self, name):
-        return 'Hello {}!'.format(name)
+        full_name = self.title_builder.build_name(name=name)
+        return 'Hello {}!'.format(full_name)
+
+
+class NameBuilder(object):
+    name = 'title_builder'
+
+    available_titles = [
+        'The Great',
+        'The Destroyer',
+        'The Wise',
+        'The Grim',
+    ]
+
+    @rpc
+    def build_name(self, name):
+        title = random.choice(self.available_titles)
+
+        return '{} {}'.format(name, title)
